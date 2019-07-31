fa5e05e765a7654a7f670c42af6e7adb07f9d62a
Mikkel Ricky <rimi@aarhus.dk>
2019-07-31 10:53:12 +0200

    Cleaned up

---
+++ aarhus_kommune_management.api.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by aarhus_kommune_management.
+ */
+
+/**
+ * List all active (non-deleted) users.
+ *
+ * @return object[]
+ *   The user list.
+ */
+function hook_aarhus_kommune_management_user_list() {
+
+}
+
+/**
+ * Create user.
+ *
+ * @param array $data
+ *   The user data.
+ *
+ * @return mixed
+ *   @TODO: What to return?
+ */
+function hook_aarhus_kommune_management_user_create($data) {
+
+}
+
+/**
+ * Update user.
+ *
+ * @param object $user
+ *   The user.
+ * @param array $data
+ *   The user data.
+ *
+ * @return mixed
+ *   @TODO: What to return?
+ */
+function hook_aarhus_kommune_management_user_update($user, $data) {
+
+}
+
+/**
+ * Delete user.
+ *
+ * @param object $user
+ *   The user.
+ *
+ * @return mixed
+ *   @TODO: What to return?
+ */
+function hook_aarhus_kommune_management_user_delete($user) {
+
+}
+
+/**
+ * Serialize user.
+ *
+ * @param object $user
+ *   The user.
+ *
+ * @return array
+ *   The serialized user data.
+ *
+ * @return mixed
+ *   @TODO: What to return?
+ */
+function hook_aarhus_kommune_management_user_serialize($user) {
+
+}
--- src/Service/UserManager.php
+++ src/Service/UserManager.php
@@ -6,6 +6,9 @@ namespace Drupal\aarhus_kommune_management\Service;
  * User manager.
  */
 class UserManager {
+  const UUID_FIELD_NAME = 'aarhus_kommune_management_uuid';
+  const NOT_IMPLEMENTED = 'aarhus_kommune_management_not_implemented';
+
   /**
    * Key => user property.
    *
@@ -20,13 +23,24 @@ class UserManager {
    * Get users.
    */
   public function getUsers() {
-    return user_load_multiple([], ['status' => 1]);
+    return $this->loadUsers();
   }
 
   /**
    * Create user.
    */
   public function createUser(array $data) {
+    if (!isset($data['uuid'])) {
+      return 'invalid data';
+    }
+
+    $hook = 'aarhus_kommune_management_user_create';
+    if (!empty(module_implements($hook))) {
+      return module_invoke_all($hook, $data);
+    }
+
+    return [$data['uuid'] => 'Do not know how to create user'];
+
     $user = (object) [];
 
     foreach (self::$properties as $key => $property) {
@@ -48,40 +62,68 @@ class UserManager {
    * Update user.
    */
   public function updateUser(array $data) {
-    if (isset($data['uuid'])) {
-      $uid = preg_replace('/^user:/', '', $data['uuid']);
-      $user = user_load($uid);
-
-      if (!$user) {
-        return [$data['uuid'] => 'No such user'];
-      }
-
-      foreach (self::$properties as $key => $property) {
-        if (isset($data[$key])) {
-          $user->{$property} = $data[$key];
-        }
-      }
-
-      $result = user_save($user);
-      return [$data['uuid'] => FALSE === $result ? 'Error updating user' : 'User updated'];
+    if (!isset($data['uuid'])) {
+      return 'invalid data';
     }
 
-    return 'invalid data';
+    $user = self::loadUserByUuid($data['uuid']);
+
+    if (FALSE === $user) {
+      return [$data['uuid'] => 'No such user'];
+    }
+
+    $hook = 'aarhus_kommune_management_user_update';
+    if (!empty(module_implements($hook))) {
+      return module_invoke_all($hook, $user, $data);
+    }
+
+    return [$data['uuid'] => 'Do not know how to update user'];
+
+    foreach (self::$properties as $key => $property) {
+      if (isset($data[$key])) {
+        $user->{$property} = $data[$key];
+      }
+    }
+
+    $result = user_save($user);
+    return [$data['uuid'] => FALSE === $result ? 'Error updating user' : 'User updated'];
   }
 
   /**
    * Delete user.
    */
   public function deleteUser(array $data) {
-    throw new \RuntimeException(__METHOD__ . ' not implemented');
+    if (!isset($data['uuid'])) {
+      return 'invalid data';
+    }
+
+    $user = self::loadUserByUuid($data['uuid']);
+
+    if (FALSE === $user) {
+      return [$data['uuid'] => 'No such user'];
+    }
+
+    $hook = 'aarhus_kommune_management_user_delete';
+    if (!empty(module_implements($hook))) {
+      return module_invoke_all($hook, $user);
+    }
+
+    return [$data['uuid'] => 'Do not know how to delete user'];
+
+    // user_delete($user);
   }
 
   /**
    * Serialize user.
    */
   public function serializeUser($user) {
+    $hook = 'aarhus_kommune_management_user_serialize';
+    if (!empty(module_implements($hook))) {
+      return module_invoke_all($hook, $user);
+    }
+
     $data = [
-      'uuid' => 'user:' . $user->uid,
+      'uuid' => $user->aarhus_kommune_management_uuid ?? NULL,
     ];
 
     foreach (self::$properties as $key => $property) {
@@ -89,6 +131,90 @@ class UserManager {
     }
 
     return $data;
+  }
+
+  /**
+   * Load users by uids.
+   *
+   * @param array $uids
+   *   The uids.
+   */
+  public function loadUsers(array $uids = []) {
+    $hook = 'aarhus_kommune_management_user_list';
+    if (!empty(module_implements($hook))) {
+      return module_invoke_all($hook);
+    }
+
+    $users = user_load_multiple($uids, ['status' => 1]);
+
+    return $this->addUuids($users);
+  }
+
+  /**
+   * Load users by uuids.
+   *
+   * @param array $uuids
+   *   The uuids.
+   *
+   * @return object[]
+   *   The users.
+   */
+  public static function loadUsersByUuid(array $uuids) {
+    $map = db_select('aarhus_kommune_management_users', 'u')
+      ->condition('u.uuid', $uuids)
+      ->fields('u', ['uid', 'uuid'])
+      ->execute()
+      ->fetchAllKeyed();
+
+    $users = user_load_multiple(array_keys($map));
+
+    return self::addUuids($users, $map);
+  }
+
+  /**
+   * Load a user by uuid.
+   *
+   * @param string $uuid
+   *   The uuid.
+   *
+   * @return bool|mixed
+   *   The user if found.
+   */
+  public static function loadUserByUuid($uuid) {
+    $users = self::loadUsersByUuid([$uuid]);
+
+    return 1 === \count($users) ? reset($users) : FALSE;
+  }
+
+  /**
+   * Add uuids to user objects.
+   *
+   * @param object[] $users
+   *   The users.
+   * @param array|null $map
+   *   An optional map from user uid to user uuid.
+   *
+   * @return object[]
+   *   The enriched users.
+   */
+  public static function addUuids(array $users, array $map = NULL) {
+    $uids = array_map(function ($user) {
+      return $user->uid;
+    }, $users);
+
+    if (NULL === $map) {
+      $map = db_select('aarhus_kommune_management_users', 'u')
+        ->condition('u.uid', $uids)
+        ->fields('u', ['uid', 'uuid'])
+        ->execute()
+        ->fetchAllKeyed();
+    }
+
+    foreach ($users as $user) {
+      $user->aarhus_kommune_management_uuid = $map[$user->uid] ?? NULL;
+    }
+
+    return $users;
   }
 
 }
